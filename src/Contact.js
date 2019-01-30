475c885e09cf7b5b90e1789c792d131d6ef2fc36
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-01-30 16:06:52 +0100

    refactor: JSDoc types

--- src/Contact.js
+++ src/Contact.js
@@ -6,7 +6,7 @@ const EmailValidator = require("email-validator");
  * @doc https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#contactObject
  *
  * @property {String} name The identifying name of the contact person/organization.
- * @property {String} url The URL pointing to the contact information. MUST be in the format of a URL.
+ * @property {URL} url The URL pointing to the contact information. MUST be in the format of a URL.
  * @property {String} email The email address of the contact person/organization. MUST be in the format of an email address.
  */
 class Contact {
@@ -14,7 +14,7 @@ class Contact {
      * @constructor
      * @memberof Contact#
      * @param {!String} name The identifying name of the contact person/organization.
-     * @param {!String} url The URL pointing to the contact information. MUST be in the format of a URL.
+     * @param {!String | URL} url The URL pointing to the contact information. MUST be in the format of a URL.
      * @param {!String} email email The email address of the contact person/organization. MUST be in the format of an email address.
      *
      * @throws {TypeError}
